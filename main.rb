1711f27604c01ff9f2903866b9ad994dbce87b8b
Daiki Ikeda <tetra.chan.da.z.1000@gmail.com>
2019-05-21 12:49:53 +0900

    Fix regexp

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ contents = doc.css('ul li').map do |node|
   node.inner_text
 end
 
-i3 = contents.select { |content| content.match?(/Mac Mini .*(クアッド|6)コア.* Core i(3|5)/) }
+i3 = contents.select { |content| content.match?(/mini[\S\s]*(クアッド|6)コア[\S\s]*i(3|5)/i) }
 
 i3.each do |content|
   `curl -X POST -H 'Content-type: application/json' --data '{"text":"#{content}"}' https://hooks.slack.com/services/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`
